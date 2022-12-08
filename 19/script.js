67c7f0a97ad7a1ff66ec896fcd07f66f824a2d75
stepanetsaleksandr <stepanetsaleksandr@gmail.com>
2022-12-08 09:42:58 +0200

    movies

--- 19/script.js
+++ 19/script.js
@@ -1,1 +1,19 @@
-console.log("awedwqewqe");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
